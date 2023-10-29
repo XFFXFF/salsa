1d11fb99a45d0153553aced3bb902c2657d9f553
XFFXFF <1247714429@qq.com>
2023-10-29 09:03:18 +0800

    .

---
+++ book/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Store the current branch or commit
+original_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$original_branch" == "HEAD" ]; then
+  original_branch=$(git rev-parse HEAD)
+fi
+
+mkdir -p versions  # Create a root directory for all versions
+
+# Declare an associative array to map commits to custom version directory names
+declare -A commit_to_version=( ["master"]="salsa2022" ["754eea8b5f8a31b1100ba313d59e41260b494225"]="salsa" )
+
+# Loop over the keys (commit hashes) in the associative array
+for commit in "${!commit_to_version[@]}"; do
+  git checkout $commit
+  mdbook build
+  version_dir="versions/${commit_to_version[$commit]}"
+  mv book $version_dir
+done
+
+# Return to the original branch or commit
+git checkout $original_branch
